edd7cac9bd18008ef4941791033f106b92cb83d8
khaoula-berrah <berah.khaoula@gmail.com>
2023-05-14 07:28:15 -0700

    simple_shell_khaoula

--- counters.c
+++ counters.c
@@ -1,25 +1,25 @@
 #include "header_file.h"
 
 /**
- * count_tokens - function that count the commands in a line.
- * @line: pointer to string that contains the commands.
- * Return: number of commands.
+ * count_tokens - Function that count the commands in a line.
+ * @line: Pointer to string that contains the commands.
+ * Return: Number of commands.
  */
 int count_tokens(char *line)
 {
-    int i, found = 1, count = 0;
+	int i, found = 1, count = 0;
 
-    while (line[i])
-    {
-        if (line[i] == ' ' || line[i] == 10 || line == '\t')
-            found = 1;
-        else if (found)
-        {
-            found = 0;
-            ++count;
-        }
-        i++;
-    }
+	while (line[i])
+	{
+		if (line[i] == ' ' || line[i] == 10 || line == '\t')
+			found = 1;
+		else if (found)
+		{
+			found = 0;
+			++count;
+		}
+		i++;
+	}
 
-    return (count);
+	return (count);
 }
