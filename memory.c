b09ccf8a496c0632f781ed89242a5ec481a181e4
khaoula-berrah <berah.khaoula@gmail.com>
2023-05-14 06:45:54 -0700

    simple_shell

--- memory.c
+++ memory.c
@@ -1,11 +1,11 @@
 #include "header_file.h"
 
 /**
- * free_parser - function that frees the pointers that is used in line_parser.
- * @line: pointer to string that contains commands.
- * @newline: copy of @line.
- * @ar: array of pointers that contains each command in @line.
- * @array: the same of @ar.
+ * free_parser - Function that frees the pointers that is used in line_parser.
+ * @line: Pointer to string that contains commands.
+ * @newline: Copy of @line.
+ * @ar: Array of pointers that contains each command in @line.
+ * @array: The same of @ar.
  */
 void free_parser(char *line, char *newline, char **ar, char **array)
 {
