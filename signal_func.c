de2781b5c654cba4a7b36a633a35b8ef75f6d329
khaoula-berrah <berah.khaoula@gmail.com>
2023-05-24 06:18:15 -0700

    test

--- signal_func.c
+++ signal_func.c
@@ -1,10 +1,10 @@
 #include "header_file.h"
 
 /**
- * signal_stop - Function that checks the signal of kayboard keys
- * if signal was a CTRL + C stop the program.
- * @signal: the signal of the keyboard keys pressed.
- * Return: void.
+ * signal_stop - Function that checks the signal of kayboard keys.
+ * If signal was a CTRL + C stop the program.
+ * @signal: The signal of the keyboard keys pressed.
+ * Return: Void.
  */
 void signal_stop(int signal)
 {
